e70d5c67ce39ac61764e5e0d00ab3ee463ca425f
Jo Minji <mingi2687@gamil.com>
2020-10-20 16:04:34 +0900

    Updates

---
+++ precache-manifest.a8a288c9711554e825535a3f6ead8ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a5641679c6d0bff55b2939c6c276ae",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "c87f891f8e622cf6fd86",
+    "url": "/react_basic_habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "ccf22451fcbe65444197",
+    "url": "/react_basic_habit_tracker/static/js/2.5a64e5f2.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/react_basic_habit_tracker/static/js/2.5a64e5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87f891f8e622cf6fd86",
+    "url": "/react_basic_habit_tracker/static/js/main.a96eb455.chunk.js"
+  },
+  {
+    "revision": "cce1584d664dd6fcc7a5",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.929c0c59.js"
+  }
+]);
